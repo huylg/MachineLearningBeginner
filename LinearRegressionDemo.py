821902cae4488a9d1672f99061345c091127c01a
huylg <huylg@vng.com.vn>
2020-03-12 15:18:33 +0700

    first commit

---
+++ LinearRegressionDemo.py
@@ -0,0 +1,11 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+x = np.array([5,15,25,35,45,55]).reshape((-1,1))
+y = np.array([5,20,14,32,22,38])
+
+model = LinearRegression()
+model.fit(x,y)
+
+r_sq = model.score(x,y)
+print(r_sq)
